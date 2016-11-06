79f3391205e8f8355846f6aff181e62c19ea7694
Andreas Hultgren <me@andreashultgren.se>
2016-11-06 21:53:34 +0100

    A playable game

--- src/Main.elm
+++ src/Main.elm
@@ -1,16 +1,45 @@
 module Gomoku exposing (..)
 
-import Html.App exposing (program)
+import Dict exposing (Dict)
+import Maybe.Extra exposing (join)
 import Html exposing (Html, text, div)
-import Collage exposing (Form, collage, move, filled, rect, group)
-import Color exposing (rgb)
+import Html.App exposing (program)
+import Html.Events exposing (on)
+import Json.Decode as Decode exposing (Decoder)
+import Collage exposing (Form, collage, move, filled, rect, oval, group)
+import Color exposing (rgb, rgba)
 import Element exposing (toHtml)
 
 
 type alias Model =
     { width : Float
     , height : Float
+    , gridSize : Int
+    , marks : Marks
+    , currentPlayer : Player
     }
+
+
+type alias Marks =
+    Dict Int Row
+
+
+type alias Row =
+    Dict Int Mark
+
+
+type Msg
+    = TileClick ( Int, Int )
+
+
+type Mark
+    = EmptyTile
+    | TakenTile Player
+
+
+type Player
+    = PlayerOne
+    | PlayerTwo
 
 
 main : Program Never
@@ -19,23 +48,100 @@ main =
         { init = init
         , update = update
         , view = view
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
+
+
+subscriptions : Model -> Sub Msg
+subscriptions x =
+    Sub.none
+
+
+generateEmptyBoard : Int -> Marks
+generateEmptyBoard count =
+    Dict.fromList <|
+        List.indexedMap (,) <|
+            List.repeat count <|
+                Dict.fromList <|
+                    List.indexedMap (,) <|
+                        List.repeat count EmptyTile
+
+
+initGridSize : Int
+initGridSize =
+    19
 
 
 init : ( Model, Cmd x )
 init =
-    ( { width = 500, height = 500 }, Cmd.none )
+    ( { width = 500
+      , height = 500
+      , gridSize = initGridSize
+      , marks = generateEmptyBoard initGridSize
+      , currentPlayer = PlayerOne
+      }
+    , Cmd.none
+    )
 
 
-update : a -> Model -> ( Model, Cmd a )
-update _ model =
-    ( model, Cmd.none )
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        TileClick ( x, y ) ->
+            if x < 0 || y < 0 || x > model.gridSize || y > model.gridSize then
+                ( model, Cmd.none )
+            else
+                ( { model
+                    | marks = updateMarks ( x, y ) model.currentPlayer model.marks
+                    , currentPlayer = updatePlayer model.marks ( x, y ) model.currentPlayer
+                  }
+                , Cmd.none
+                )
 
 
-gridSize : Float
-gridSize =
-    19
+updateMarks : ( Int, Int ) -> Player -> Marks -> Marks
+updateMarks ( x, y ) player marks =
+    Dict.update x (Maybe.map <| updateRow y player) marks
+
+
+updateRow : Int -> Player -> Row -> Row
+updateRow y player row =
+    Dict.update y (Maybe.map <| updateMark player) row
+
+
+updateMark : Player -> Mark -> Mark
+updateMark player mark =
+    case mark of
+        EmptyTile ->
+            TakenTile player
+
+        TakenTile _ ->
+            mark
+
+
+updatePlayer : Marks -> ( Int, Int ) -> Player -> Player
+updatePlayer marks coords player =
+    case getMark coords marks of
+        Nothing ->
+            player
+
+        Just (TakenTile _) ->
+            player
+
+        Just EmptyTile ->
+            case player of
+                PlayerOne ->
+                    PlayerTwo
+
+                PlayerTwo ->
+                    PlayerOne
+
+
+getMark : ( Int, Int ) -> Marks -> Maybe Mark
+getMark ( x, y ) marks =
+    Dict.get x marks
+        |> Maybe.map (Dict.get y)
+        |> join
 
 
 grid : Float -> Float -> Float -> List Form
@@ -62,15 +168,89 @@ grid width height count =
         List.append verticalLines horizontalLines
 
 
-view : Model -> Html a
-view { width, height } =
-    div []
+decodeClickLocation : Decoder ( Int, Int )
+decodeClickLocation =
+    Decode.object2 (,)
+        (Decode.object2 (-)
+            (Decode.at [ "pageX" ] Decode.int)
+            (Decode.at [ "target", "offsetLeft" ] Decode.int)
+        )
+        (Decode.object2 (-)
+            (Decode.at [ "pageY" ] Decode.int)
+            (Decode.at [ "target", "offsetTop" ] Decode.int)
+        )
+
+
+coordsToTile : Float -> Float -> Float -> ( Int, Int ) -> ( Int, Int )
+coordsToTile width height count ( x, y ) =
+    ( floor ((toFloat x) / width * count), floor ((toFloat y) / height * count) )
+
+
+view : Model -> Html Msg
+view model =
+    div [ on "click" (Decode.map (coordsToTile model.width model.height (toFloat model.gridSize) >> TileClick) decodeClickLocation) ]
         [ toHtml <|
             collage
-                (round width)
-                (round height)
-                [ move ( 0, 0 ) <|
-                    group <|
-                        grid width height gridSize
+                (round model.width)
+                (round model.height)
+                [ group <|
+                    grid model.width model.height (toFloat model.gridSize)
+                , marksView model
                 ]
+        , playerView model
         ]
+
+
+playerView : Model -> Html Msg
+playerView { currentPlayer } =
+    case currentPlayer of
+        PlayerOne ->
+            text "Turn: Black"
+
+        PlayerTwo ->
+            text "Turn: White"
+
+
+marksView : Model -> Form
+marksView model =
+    Dict.map (rowView model) model.marks
+        |> Dict.values
+        |> group
+
+
+rowView : Model -> Int -> Row -> Form
+rowView model x row =
+    Dict.map (markView model x) row
+        |> Dict.values
+        |> group
+
+
+markView : Model -> Int -> Int -> Mark -> Form
+markView { width, height, gridSize } x y mark =
+    let
+        cellWidth =
+            (width / (toFloat gridSize))
+
+        cellHeight =
+            (height / (toFloat gridSize))
+
+        pos =
+            move
+                ( cellWidth * (toFloat x) - width / 2 + cellWidth / 2
+                , cellHeight * (toFloat -y) + height / 2 - cellHeight / 2
+                )
+
+        shape =
+            oval 10 10
+    in
+        case mark of
+            EmptyTile ->
+                pos <| filled (rgba 0 0 0 0) shape
+
+            TakenTile player ->
+                case player of
+                    PlayerOne ->
+                        pos <| filled (rgb 0 0 0) shape
+
+                    PlayerTwo ->
+                        pos <| filled (rgb 255 255 255) shape
